1e628df30196c1778fef0a2181685d78b626a398
murpman300 <jeanlouismurphy@gmail.com>
2017-09-18 16:29:48 -0400

    Server File

--- server.js
+++ server.js
@@ -4,45 +4,95 @@ let TLS = snk.tlsHost;
 let TCP = snk.socket;
 const fs = require('fs');
 
-/*let host = new TLS({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.crt'),
-    ca: [ fs.readFileSync('CAchain.pem') ]
-});
 
-host.on('protect', function (data) {
-    console.log(data);
-});
+String.prototype.parseFor = function (value, applyTo, result) {
+    let index = this.split(value);
+    if (index.length === 2) {
+        if (applyTo) {
+            if (index[0] === "port") {
+                applyTo[index[0]] = Number(index[1]);
+            } else {
+                applyTo[index[0]] = String(index[1]);
+            }
+            return
+        }
+        let ob = Object();
+        ob[index[0]] = index[1];
+        result(ob);
+    }
+};
 
-host.start(8000);*/
+process.env.TYPE_PROCESS = "MAIN";
 
-async function Begin() {
+function Perform() {
+    return new Promise(function (finished) {
+        fs.readFile('./serverConfig.json', async function (err, data) {
+            if (err) {
+                if (err.toString().indexOf("ENOENT: no such file or directory") !== -1) {
+                    //run some script to load the data or to apply from another js file.
+                }
+                return finished(Promise.reject())
+            }
 
+            try {
+                let config = await JSON.parse(data);
+
+                if (config.port && typeof config.port === 'number') await processFrom("port", config.port);
+
+                if (config.host && typeof config.host === 'string') await processFrom("host", config.host);
+                return finished(Promise.resolve())
+            } catch(e) {
+                return finished(Promise.reject(e))
+            }
+            async function processFrom(key, value) {
+                switch (key) {
+                    case "port":
+                        process.env.port = value;
+                        break;
+                    case "host":
+                        process.env.host = value;
+                        break;
+                    default:
+                        break
+                }
+            }
+        })
+    })
+
+}
+
+function StartServer() {
+    console.log('Starting...');
     try {
-
-        let file = await fs.readFileSync('serverConfi.json');
-
-        let config = JSON.parse(file);
-
-        if (!config.port) throw 'Missing PORT';
-
-        if (!config.host) throw 'Missing host';
-
-        let host = new TCP();
-
-        host.listen(8000);
-
+        let start = Begin();
+        process.env.HAS_STARTED = "yes"
     } catch(e) {
-        throw "Begin ERROR : " + e;
+        console.log(e);
+    }
+
+    async function Begin() {
+        try {
+
+            let host = new TCP();
+
+            host.listen(process.env.port);
+
+        } catch(e) {
+            throw "Begin ERROR : " + e;
+        }
     }
 }
 
-
-try {
-
-    let start = Begin();
-
-} catch(e) {
-    console.log(e);
+async function Go() {
+    try {
+        await Perform();
+        StartServer();
+    } catch(e) {
+        console.log(e);
+    }
 }
+
+let t = Go();
+
+
 

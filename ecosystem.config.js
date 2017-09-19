8dc998116db3d21b48bd7b2bac46e0162958a06d
murpman300 <jeanlouismurphy@gmail.com>
2017-09-19 16:41:11 -0400

    Added PM2 Config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
         production: {
             user: 'ubuntu',
             host: 'ec2-34-228-59-249.compute-1.amazonaws.com',
-            key: '~/.ssh/id_rsa',
+            key: '~/.ssh/id_rsa.pem',
             ref: 'origin/master',
             repo: 'git@github.com:murphman300/ServerTests.git',
             path: '/home/ubuntu/ServerTests',
